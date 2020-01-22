253be7c4d3b1c57a9da937b213f8396f30d548a9
HoangNgocSonz <hoangson20bg@gmail.com>
2020-01-22 22:56:18 +0700

    k

---
+++ MangaZ/public/precache-manifest.c614291142cfc7c63217736d9f8682c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff09004d7f0d93e558d151c8cb738889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474c9e7921e9938a1f17",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ce67fa253cd2e5dccfb0",
+    "url": "/static/css/main.4a18454e.chunk.css"
+  },
+  {
+    "revision": "474c9e7921e9938a1f17",
+    "url": "/static/js/2.ea4c85b4.chunk.js"
+  },
+  {
+    "revision": "8872f866615ae79ad709edf692723712",
+    "url": "/static/js/2.ea4c85b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce67fa253cd2e5dccfb0",
+    "url": "/static/js/main.f889806c.chunk.js"
+  },
+  {
+    "revision": "49f0baeb94ca3e12acc5",
+    "url": "/static/js/runtime-main.24eacbb2.js"
+  }
+]);
